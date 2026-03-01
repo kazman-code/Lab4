8927ab301eb1726cb4085b4d5cdf071a972a377e
Mick_code <mickdonnellan2@gmail.com>
2026-03-01 16:08:08 +0000

    Update app.ts
    comments

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,17 +4,22 @@ import { ListComponent } from './list-component/list-component';
 
 @Component({
   selector: 'app-root',
+  //imports
   imports: [RouterOutlet, ListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  //signal notifies angular when it changes
   protected readonly title = signal('data-app');
-  count: number = 0;
-  hide: boolean = false;
+  //Declaration of Vaiables
+  count: number = 0;//amount of times button  has been clicked
+  hide: boolean = false;//used to show if image is hidden or shown
+  //increments counter with each button click
   OnClickButton() {
     this.count++;
   } 
+  //hides and shows the image when double clicked
   onImageDoubleClick(){
     if(this.hide == false) {
       this.hide = true;
