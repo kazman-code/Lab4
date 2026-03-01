10b1fcdd23e0283cff8a168174057240e9125283
Mick_code <mickdonnellan2@gmail.com>
2026-03-01 16:27:37 +0000

    Update list-component.ts

--- src/app/list-component/list-component.ts
+++ src/app/list-component/list-component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';//imports angular
 
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//connects to html
   imports: [],
-  templateUrl: './list-component.html',
-  styleUrl: './list-component.css',
+  templateUrl: './list-component.html',//html
+  styleUrl: './list-component.css',/css
 })
 export class ListComponent {
+  //array of presidents
   presidents: string[] = ["Higgins", "McAleese", "Robinson", "Hillery"];
 
+  //array of objects holding the name of presidents and the term
   mypresidents: any[] =
 [{name: "Higgins", term:"2011-2025"},
 {name: "McAleese", term:"1997-2011"},
